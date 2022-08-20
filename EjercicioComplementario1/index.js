705b90987c9613aae687a21c81ce503d4a53db2a
Bruno Iglesias <iglesiasb11@gmail.com>
2022-08-20 15:50:38 -0300

    Ejercicio Complementario 1

--- EjercicioComplementario1/index.js
+++ EjercicioComplementario1/index.js
@@ -1,53 +1,3 @@
-/* comentario de linea */
-/* Comentario de bloque */
-/* Declaracion de variable
-let nombre;
-
-Definición de variable
-nombre = "andres";
-
-
-let edad = 5; */
-/* Cuando aplicamos const, el valor quedará fijo, mientras que con let no habrá problema */
-/* Todas las variables es conveniente declarar en minúscula */
-/* Declarar utilizando camelCase */
-/* let EDAD = 5;
-
-let numeroUnoBonito
- */
-
-/* let numeroUno = 3;
-
-let numeroDos = 7;
-
-let suma = numeroUno + numeroDos;
-console.log(suma); */
-/* let saludo = "Hola";
-let nombre = "Gonza";
-let mensaje = saludo + " " + nombre;
-/* En string el + funciona como concatenación */
-/*console.log("===================================", mensaje); */
-
-/* let edad = 10;
-let nombre = "andres";
-console.log("Bienvenido señor " + nombre + " tienes " + edad + " años"); */
-/* let nombre = prompt("Ingrese el nombre");
-console.log("Hola " + nombre); */
-
-/* Typeof nos muestra el tipo de variable que usamos */
-/* let nombre = "andres";
-let edad = 8;
-console.log(typeof(nombre, edad)); */
-
-/* let nombre = prompt ("Ingrese su nombre");
-let edad = prompt("Ingrese su edad");
-
-let mensaje = "Bienvenid@ " + nombre + " tienes " + edad + " años";
-alert(mensaje); */
-
-/* let entrada = prompt("Ingresar una letra");
-let salida = entrada + " " + "Ingresada";
-alert(salida); */
 let entrada=true
 while (entrada) {
 let numeroUno = prompt("Ingrese un numero");
@@ -78,6 +28,11 @@ for(let i = 0; i <= 10; i++){
   }
   
 let salida = confirm("Quiere repetir?");
-if (salida === flase) {
-    break;}
+switch (salida){
+    case true:
+    break;
+    case false:
+        entrada = false;
+        break;
+}
 }
--- ejercicio1/index.js
+++ ejercicio1/index.js
@@ -21,3 +21,55 @@ if (intervalo >=10 && intervalo<=50) {
 }else {
     alert ("Su número es mayor a 50");
 }
+
+
+/* comentario de linea */
+/* Comentario de bloque */
+/* Declaracion de variable
+let nombre;
+
+Definición de variable
+nombre = "andres";
+
+
+let edad = 5; */
+/* Cuando aplicamos const, el valor quedará fijo, mientras que con let no habrá problema */
+/* Todas las variables es conveniente declarar en minúscula */
+/* Declarar utilizando camelCase */
+/* let EDAD = 5;
+
+let numeroUnoBonito
+ */
+
+/* let numeroUno = 3;
+
+let numeroDos = 7;
+
+let suma = numeroUno + numeroDos;
+console.log(suma); */
+/* let saludo = "Hola";
+let nombre = "Gonza";
+let mensaje = saludo + " " + nombre;
+/* En string el + funciona como concatenación */
+/*console.log("===================================", mensaje); */
+
+/* let edad = 10;
+let nombre = "andres";
+console.log("Bienvenido señor " + nombre + " tienes " + edad + " años"); */
+/* let nombre = prompt("Ingrese el nombre");
+console.log("Hola " + nombre); */
+
+/* Typeof nos muestra el tipo de variable que usamos */
+/* let nombre = "andres";
+let edad = 8;
+console.log(typeof(nombre, edad)); */
+
+/* let nombre = prompt ("Ingrese su nombre");
+let edad = prompt("Ingrese su edad");
+
+let mensaje = "Bienvenid@ " + nombre + " tienes " + edad + " años";
+alert(mensaje); */
+
+/* let entrada = prompt("Ingresar una letra");
+let salida = entrada + " " + "Ingresada";
+alert(salida); */
