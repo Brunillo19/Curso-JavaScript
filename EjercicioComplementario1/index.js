f69ca7b817f9fe11137da1c5be64c1badf31d550
Bruno Iglesias <iglesiasb11@gmail.com>
2022-08-20 17:05:29 -0300

    Ejercicio Complementario 1

--- EjercicioComplementario1/index.js
+++ EjercicioComplementario1/index.js
@@ -1,25 +1,36 @@
 let entrada=true
 while (entrada) {
-let numeroUno = prompt("Ingrese un numero");
-let numeroDos = prompt("Ingrese otro numero");
+let numeroUno = parseFloat(prompt("Ingrese un numero"));
+if (isNaN(numeroUno) || numeroUno=="") {
+    alert("Ingrese un numero");
+    let numeroUno = parseFloat(prompt("Ingrese un numero"));
+}
+let numeroDos = parseFloat(prompt("Ingrese otro numero"));
+if (isNaN(numeroDos) || numeroDos=="") {
+    alert("Ingrese un numero");
+    let numeroDos = parseFloat(prompt("Ingrese un numero"));
+}
+
 let Potencia = confirm("Querés saber la potencia?");
 if (Potencia === true) {
 
-let mensaje1 = parseInt(numeroUno) ** parseInt(numeroDos);
+let mensaje1 = parseFloat(numeroUno) ** parseFloat(numeroDos);
 alert(mensaje1);}
 let Suma = confirm("Querés saber la suma?");
 if (Suma === true) {
-let mensaje2 = parseInt(numeroUno) + parseInt(numeroDos);
+let mensaje2 = parseFloat(numeroUno) + parseFloat(numeroDos);
 alert(mensaje2);}
 let Resta = confirm("Querés saber la resta?");
 if (Resta === true) {
-    let mensaje3 = parseInt(numeroUno) - parseInt(numeroDos);
+    let mensaje3 = parseFloat(numeroUno) - parseFloat(numeroDos);
     alert(mensaje3);}
 
 
 
 
 let numero = prompt("Ingrese el número que quiere saber la tabla");
+if (numero === "ESC"){alert("Ingrese un número");
+let numero = prompt("Ingrese el número que quiere saber la tabla");}
 for(let i = 0; i <= 10; i++){
     let multiplicacion = numero * i;
     let mensaje = `${numero} * ${i} = ${multiplicacion}`;
