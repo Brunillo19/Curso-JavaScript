ee26fe1bf0e32d284f53b8188a10404979333412
Bruno Iglesias <iglesiasb11@gmail.com>
2022-11-06 02:20:00 -0300

    Proyecto Final

--- ProyectoFinal/index.js
+++ ProyectoFinal/index.js
@@ -27,12 +27,13 @@ function generar() {
   let obra = document.getElementById("obra").checked;
   let costo = parseFloat(document.getElementById("costo").value);
   let dia = document.getElementById("dia").value;
-  costo=obra ? (costo = costo * 0.6.toFixed(2)) : (costo = costo);
+  costo=obra ? ((costo = (costo * 0.6).toFixed(2))) : (costo = costo);
   (nomPa==""||hora==""||tratamiento==""||costo==""||dia=="")?alert("Ingrese todos los datos solicitados"):
   (localStorage.getItem("turnos", JSON.stringify(tablaturno)),
   tablaturno.push(
     new turno(nomPa, hora, comentario, tratamiento, obra, costo, dia)
   ),
+  localStorage.setItem("turnos", JSON.stringify(tablaturno)),
   document.getElementById("nombrePaciente").value = "",
   document.getElementById("hora").value = "",
   document.getElementById("comentario").value = "",
@@ -51,56 +52,37 @@ function generar() {
 }
 
 
-function creoturno() {
-  let div = document.createElement("div");
-  let button = document.createElement("input");
-/* Formato TURNOS */
-   div.className = "turnos";
-  /* tablaturno.forEach((item,idx) => {
-    div.innerHTML = `
-          <div class="card-body text-bg-secondary">
-          <h6 class="card-subtitle mb-2 ">Horario del turno: ${item.hora}</h6>
-          <p>Nombre Paciente: ${item.nomPa}</p>
-          <p>Comentario Adicional: ${item.comentario}</p>
-          <p>Tratamiento: ${item.tratamiento}</p>
-          <p>Costo final: ${item.costo}</p>
-          <p>Día del turno: ${item.dia}</p>
-          <button class="eliminar btn btn-light text-bg-secondary" id="${idx}"> Eliminar Turno </button>
-          </div>
-          `;
-  });  */
 
-  localStorage.setItem("turnos", JSON.stringify(tablaturno));
-   button.type = "button";
-  button.id = "eliminar";
-  button.value = "Eliminar Turno";
-  button.className = "eliminar btn btn-light text-bg-secondary"; 
-
-  button.addEventListener("click", (idx) => {
-    Swal.fire({
-      title: "Está seguro de eliminar el turno?",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonText: "Sí, seguro",
-      cancelButtonText: "No, no quiero",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire({
-          title: "Borrado!",
-          icon: "success",
-          text: "El turno ha sido eliminado",
-        });
-        console.log(tablaturno.idx);
-        tablaturno.splice(idx,1);
-        contenido.removeChild(div);
-      }
-    });
-  } );
-
-   
-}
-function elimino() {
+function eliminoTurno(e) {
   let div = document.getElementById("turnosGenerados");
+  let turnos = JSON.parse(localStorage.getItem("turnos"));
+  Swal.fire({
+    title: "Está seguro de eliminar el turno?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Sí, seguro",
+    cancelButtonText: "No, no quiero",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      //logica para eleminar del carrito
+      console.log(turnos.nomPa);
+      
+      
+      /* const borrar = turnos.find( ,id) => id=idx; */
+      /* turnos.splice(0,1); */
+
+      div.innerHTML="";
+      Swal.fire({
+        title: "Borrado!",
+        icon: "success",
+        text: "El turno ha sido eliminado",
+      });
+    }
+  });
+} 
+function eliminoLista() {
+  let div = document.getElementById("turnosGenerados");
+  let turnos = JSON.parse(localStorage.getItem("turnos"));
   Swal.fire({
     title: "Está seguro de eliminar todos los turnos?",
     icon: "warning",
@@ -110,7 +92,9 @@ function elimino() {
   }).then((result) => {
     if (result.isConfirmed) {
       //logica para eleminar del carrito
-      tablaturno.splice(0);
+      console.log(turnos);
+      turnos.splice(0);
+      localStorage.setItem("turnos", JSON.stringify(turnos));
       div.innerHTML="";
       Swal.fire({
         title: "Borrado!",
@@ -121,16 +105,15 @@ function elimino() {
   });
 };
 limpiar && limpiar.addEventListener("click", () => {
-  elimino();});
+  eliminoLista();});
 
 genero && genero.addEventListener("click", (e) => {
   e.preventDefault();
 
   generar();
-  creoturno();
-  //  setTimeout(() => {
-  //    location.href="./index.html";
-  //  }, 8000);
+    setTimeout(() => {
+      location.href="./index.html";
+    }, 8000);
   // /* Hago función que recolecte los datos y en otra función genero la card */
 
 
@@ -138,62 +121,29 @@ genero && genero.addEventListener("click", (e) => {
 
 
 
-eliminar && eliminar.addEventListener("click", e => {
-  e.preventDefault();
-  const id = e.target.id;
-  console.log(e.target.id);
-})
+
 /* Genero una varible para mostrar los turnos en la página inicial */
 
 
-function muestro (){
+const muestro = async () =>{
 let base = JSON.parse(localStorage.getItem("turnos"));
 let div = document.createElement("div");
-let button = document.createElement("input");
 let turnosGenerados = document.getElementById("turnosGenerados");
 div.className="turnos";
-console.log(base); 
-tablaturno.forEach((item,idx) => {
-  div.innerHTML = `
-        <div class="card-body text-bg-secondary">
-        <h6 class="card-subtitle mb-2 ">Horario del turno: ${item.hora}</h6>
-        <p>Nombre Paciente: ${item.nomPa}</p>
-        <p>Comentario Adicional: ${item.comentario}</p>
-        <p>Tratamiento: ${item.tratamiento}</p>
-        <p>Costo final: ${item.costo}</p>
-        <p>Día del turno: ${item.dia}</p>
-        <button class="eliminar btn btn-light text-bg-secondary" id="${idx}"> Eliminar Turno </button>
-        </div>
-        `;
-        div.appendChild(button);
-       turnosGenerados.append(div);
-        button.type = "button";
-        button.id = "eliminar";
-        button.value = "Eliminar Turno";
-        button.className = "eliminar btn btn-light text-bg-secondary"; 
-        
-        button.addEventListener("click", (idx) => {
-          Swal.fire({
-            title: "Está seguro de eliminar el turno?",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonText: "Sí, seguro",
-            cancelButtonText: "No, no quiero",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire({
-          title: "Borrado!",
-          icon: "success",
-          text: "El turno ha sido eliminado",
-        });
-        tablaturno.splice(idx);
-        turnosGenerados.removeChild(div);
-      }
-    });
-  });
-
+base.forEach((turno,idx) => {
+  div.innerHTML += `
+  <div class="turnoSingular card-body text-bg-secondary">
+  <h6 class="card-subtitle mb-2 ">Horario del turno: ${turno.hora}</h6>
+  <p>Nombre Paciente: ${turno.nomPa}</p>
+  <p>Día del turno: ${turno.dia}</p>
+  <p>Comentario Adicional: ${turno.comentario}</p>
+  <p>Tratamiento: ${turno.tratamiento}</p>
+  <p>Costo final: ${turno.costo}</p>
+  <button class="eliminar btn btn-light text-bg-secondary" id="${idx}" onclick="eliminoTurno()"> Eliminar Turno </button>
+  </div>
+  `;
+  turnosGenerados.append(div);       
 } 
 );}
-let planilla = async () => {
-  muestro();};
-  planilla();
+
+  muestro();
